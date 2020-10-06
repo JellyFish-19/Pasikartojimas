1be322709b574bf390efafcdb8a27517d196bc28
Justinas Čepas <62259726+JellyFish-19@users.noreply.github.com>
2020-10-06 16:22:32 +0300

    Update CKlase.cs

--- PASIKARTOJIMAS/CKlase.cs
+++ PASIKARTOJIMAS/CKlase.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace PASIKARTOJIMAS
 {
-    class CKlase
+    class CKlase : BKlase
     {
+        public int Number { get; set; }
+        public string Word { get; set; }
+        public double DoubleNumber { get; set; }
+        public AKlase AObjektas {get; set;}
+
+        public CKlase(string name, int number, string word, double doubleNumber, AKlase aObjektas) : base (name)
+        {
+            Number = number;
+            Word = word;
+            DoubleNumber = doubleNumber;
+            AObjektas = aObjektas;
+        }
+
+        public CKlase(string name, int number) : base(name)
+        {
+            Number = number;
+        }
+
+        public CKlase(string name, int number, string word) : base(name)
+        {
+            Number = number;
+            Word = word;
+        }
+
+        public CKlase(string name) : base (name)
+        {
+
+        }
     }
 }
